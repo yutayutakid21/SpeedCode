da87efba73af3f0b86bf90cf5aa82134236bdb09
Hiroki Kondo <kondo.matr02@gmail.com>
2020-01-28 19:06:28 +0900

    ストーリーボードとviewControllerの紐付け完了

---
+++ SpeedCode/oneViewController.swift
@@ -0,0 +1,30 @@
+//
+//  oneViewController.swift
+//  SpeedCode
+//
+//  Created by 近藤宏輝 on 2020/01/28.
+//  Copyright © 2020 Fujii Yuta. All rights reserved.
+//
+
+import UIKit
+
+class oneViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
